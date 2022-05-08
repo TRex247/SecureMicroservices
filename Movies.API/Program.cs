9616b0b7631cf5b573dc57a1ee7caaa240014924
Tonye Roberts <talentedrex@gmail.com>
2022-05-08 16:46:52 +0100

    Add In memory seeded database for Movies API

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Movies.API", Version = "v1" });
+});
+builder.Services.AddDbContext<MoviesAPIContext>(options => options.UseInMemoryDatabase("MoviesAPIContext"));
 
 var app = builder.Build();
 
@@ -21,5 +28,15 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+SeedDatabase(app);
+
+static void SeedDatabase(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var moviesContext = services.GetRequiredService<MoviesAPIContext>();
+    MoviesContextSeed.SeedAsync(moviesContext);
+}
 
 app.Run();
